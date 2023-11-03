0b010b7769e64d2e7937d20ed4fb83ec940d4cae
kerubo99 <estherk991@gmail.com>
2023-11-03 09:16:15 +0300

    initial commit

---
+++ standard_lib.c
@@ -0,0 +1,7 @@
+#include <stdio.h> //include the standard input/output library header
+int main() 
+{
+int x = 42;
+printf("the value of x is %d\n", x); //using the printf functions from the stdio library
+return 0;
+}
