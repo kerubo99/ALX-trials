571efd3068ae0e909f197ec0b0ba161e7b9e16ba
kerubo99 <estherk991@gmail.com>
2023-12-10 14:44:17 +0300

    internal changes

---
+++ test2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+firstname = "Esther"
+lastname = "Kerubo"
+print(firstname, lastname, end="\n")
+#prints my fisrt name and last name
+print("she is", end=" ")
+print("a girl", end=" ")
+print("from kenya", end="\n")
+print("that loves coding")
+
