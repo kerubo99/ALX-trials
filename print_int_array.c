b3c35b9b585dca2a08bbc17061a755e1c2ecaa45
kerubo99 <estherk991@gmail.com>
2023-12-25 23:06:47 +0300

    today's

---
+++ print_int_array.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_int_array(int *a, int size)
+{
+    int i;
+    
+    i = 0;
+    while (i <  size)
+    {
+        printf("%d\n", a[i]);
+        i++;
+    }
+}
+
+int main(int ac, char **av)
+{
+    int *a;
+    int asize;
+    int i;
+    
+    if (ac < 2)
+    {
+        printf("Please give me at least one number\n");
+        printf("Usage: %S  number [NUMBER]\n", av[0]);
+        return (1);
+    }
+    asize = ac - 1;
+    a = malloc(asize * sizeof(int));
+    i = 0;
+    while (i < asize)
+    {
+        a[i] = atoi(av[i + 1]);
+        i++;
+    }
+    print_int_array(a, asize);
+    free(a);
+    return (0);
+}   
+ 
+            
