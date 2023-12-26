06c1593ca0c6e5ad26fcb0611501bbf3d52b69e9
kerubo99 <estherk991@gmail.com>
2023-12-26 20:41:51 +0300

    today's

---
+++ if.py
@@ -0,0 +1,22 @@
+# Example 1
+x = 10
+
+if x > 5:
+    print("x is greater than 5")
+
+# Example 2
+temperature = 25
+
+if temperature > 30:
+    print("It's a hot day")
+else:
+    print("It's not a hot day")
+
+# Example 3
+age = 18
+
+if age >= 18:
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote yet")
+
